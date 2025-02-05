e6292803657c042a5eca6a2a4304b663f8309509
Nonatosa <albertonnbz@gmail.com>
2025-02-04 20:24:59 -0600

    Entrega final con ejemplos

---
+++ tipos-de-dato.js
@@ -0,0 +1,47 @@
+//Este es un comentario en JavaScript
+
+console.log("JavaScript desde 0");
+
+/*Ejemplo en clase
+//Tipos de datos
+
+var nombre = "Ana";
+var edad = 23;
+var sueldo = 1.234;
+var casada = false;
+var vacunas = true;
+var hijos = null;
+var dato = "";
+
+//Imprimir tipos de datos
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+*/
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+
+
+console.log(typeof(1.56));
+console.log(typeof(10 == 10));
+console.log(typeof(10 >= 10));
+console.log(typeof('Hola' == 'hola'));
+console.log(typeof(false));
+
+
+
+
+
